ece7ded3781b2f7c1344b0b5d36b6930fdf91be7
Howard Miao <boredassian@yahoo.com>
2020-06-18 20:06:25 +0800

    Created line_items index, AJAX edit, and AJAX update.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,30 @@
 class LineItemsController < ApplicationController
+  before_action :set_line_items
+  before_action :set_line_item, only: %i[edit update]
+
   def index
-    @line_items = LineItem.all
+    @line_items = @line_items.page(params[:page]).per(params[:per])
+  end
+
+  def edit; end
+
+  def update
+    @line_item.update!(line_item_params)
+
+    render :show
+  end
+
+  private
+
+  def set_line_items
+    @line_items = LineItem.includes(:campaign)
+  end
+
+  def set_line_item
+    @line_item = @line_items.find(params[:id])
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:adjustments, :lock_version)
   end
 end
